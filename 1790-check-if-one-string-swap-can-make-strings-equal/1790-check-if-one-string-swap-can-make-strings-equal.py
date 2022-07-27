d4976422c08c9d5c03399e6d125d332f102f9ae6
Asif3786 <109464003+Asif3786@users.noreply.github.com>
2022-07-27 14:27:14 +0530

    Time: 42 ms (67.14%), Space: 14 MB (20.57%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -4,10 +4,7 @@ class Solution:
         for i in range(len(s1)) : 
             if s1[i] != s2[i] :
                 count=count+ 1
-        if count<=2 and sorted(s1)==sorted(s2):
-            return True
-        else:
-            return False
+        return True if count<=2 and sorted(s1)==sorted(s2) else False
             
                 
         
