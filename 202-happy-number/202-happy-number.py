750066e0b2ea6c8ac3e1431eba9bac08b0944250
Asif3786 <109464003+Asif3786@users.noreply.github.com>
2022-07-27 12:54:59 +0530

    Time: 75 ms (10.15%), Space: 13.8 MB (60.10%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -1,13 +1,13 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
         def validate(n,vis):
-            sm=0
+            sum=0
             if n==1:
                 return True
             if n in vis:
                 return False
             vis.add(n)
             for i in str(n):
-                sm+=int(i)*int(i)
-            return validate(sm,vis)
+                sum=sum+int(i)*int(i)
+            return validate(sum,vis)
         return validate(n,set())
