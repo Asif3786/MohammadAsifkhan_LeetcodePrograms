5ea61feaef6d87cb623030024aaeb0d9fb99c7e9
Asif3786 <109464003+Asif3786@users.noreply.github.com>
2022-08-10 15:25:53 +0530

    Time: 90 ms (28.66%), Space: 13.8 MB (76.66%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,16 +1,20 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        nexts = {"M": 1000,"D": 500,"C": 100,"L": 50,"X": 10,"V": 5,"I": 1,"IV": 4,
-            "IX": 9,"XL": 40,"XC": 90,"CD": 400,"CM": 900}
-        def solve(i):
-            if i == len(s):
-                return 0
-            if i == len(s) - 1:
-                return nexts[s[i]]
-            temp = ""
-            while i < len(s) and temp + s[i] in nexts:
-                temp += s[i]
-                i += 1
-            return nexts[temp] + solve(i)
-        return solve(0)
+        roman = {
+            "I" : 1,
+            "V" : 5,
+            "X" : 10,
+            "L" : 50,
+            "C" : 100,
+            "D" : 500,
+            "M" : 1000
+            }
+        n = len(s)
+        integer = 0
+        for i in range(n):
+            if  i == n-1 or roman[s[i]] >= roman[s[i+1]] :
+                integer += roman[s[i]]
+            else :
+                integer -= roman[s[i]]      
+        return integer
         
