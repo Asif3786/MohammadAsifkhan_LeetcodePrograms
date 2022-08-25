05686f99da4da89984885ffecaa518cae360f958
Asif3786 <109464003+Asif3786@users.noreply.github.com>
2022-08-25 20:00:26 +0530

    Time: 3161 ms (5.25%), Space: 0B (100.00%) - LeetHub

--- 577-employee-bonus/577-employee-bonus.sql
+++ 577-employee-bonus/577-employee-bonus.sql
@@ -1,4 +1,7 @@
 /* Write your T-SQL query statement below */
 select a.name,b.bonus from Employee a left join 
 bonus b on a.empId=b.empId 
-where b.bonus<1000 or b.bonus is null
+where b.bonus is null or b.bonus<1000
+
+
+
